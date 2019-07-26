6af79a035940c12b674d2adf13e3ea0d9b99a65b
rajunadimpalli <sraju1521@gmail.com>
2019-07-26 10:08:05 -0400

    Add project files.

---
+++ CleanArch.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,22 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.Services;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Infra.Data.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infra.IoC
+{
+    public class DependencyContainer 
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Application layer
+            services.AddScoped<ICourseService, CourseService>();
+            //Infra.Data layer
+            services.AddScoped<ICourseRepository, CourseRepository>();
+        }
+    }
+}
